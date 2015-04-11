351f287a293f3ded7a3159b34cf0c1104b0168c1
Matthew Jallouk <jalloukmat@yahoo.com>
2015-04-10 22:54:04 -0400

    Finished commenting

--- app/src/main/java/utc_4910/screenActivities/MainActivity.java
+++ app/src/main/java/utc_4910/screenActivities/MainActivity.java
@@ -13,10 +13,14 @@ import utc_4910.photoencryptionincloud.R;
  */
 public class MainActivity extends ActionBarActivity {
 
+    //Define instance variables
     private Button loginButton = null;
     private Button createAccountButton = null;
 
-
+    /** Method that defines initial properties of the screen.
+     *
+     * @param savedInstanceState                saved state of the instance
+     */
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
@@ -24,10 +28,19 @@ public class MainActivity extends ActionBarActivity {
         loginButton = (Button)findViewById(R.id.loginButton);
         createAccountButton = (Button)findViewById(R.id.createAccountButton);
 
+        //Listen for events
         this.runButtonListeners();
     }
 
+    /** Method that contains all of the cases to handle events.
+     *
+     */
     public void runButtonListeners(){
+
+        /** Function call to occur when the Login button is tapped.
+         *  We switch over to the LoginActivity screen.
+         *
+         */
         loginButton.setOnClickListener(new Button.OnClickListener(){
             public void onClick(View v){
                 Intent i = new Intent();
@@ -37,6 +50,10 @@ public class MainActivity extends ActionBarActivity {
             }
         });
 
+        /** Function call to occur when the CreateAccount button is tapped.
+         *  We switch over to the CreateAccountActivity screen.
+         *
+         */
         createAccountButton.setOnClickListener(new Button.OnClickListener(){
             public void onClick(View v){
                 Intent i = new Intent();
@@ -46,5 +63,4 @@ public class MainActivity extends ActionBarActivity {
             }
         });
     }
-
 }
